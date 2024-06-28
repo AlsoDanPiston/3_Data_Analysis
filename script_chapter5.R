8444eef0b5accfd98a6af49a2840ce60621413ca
daniel piston <djpiston@gmail.com>
2024-06-28 12:40:23 -0400

    completed chapter 5

--- script_chapter5.R
+++ script_chapter5.R
@@ -1,7 +1,30 @@
-install.packages("tidyverse")
-install.packages("gapminder")
+# install.packages("tidyverse")
+# install.packages("gapminder")
 library(gapminder)
+library(tidyverse)
 
 # display structure of element
 str(gapminder) 
+
+class(gapminder)
+gapminder
+names(gapminder)
+ncol(gapminder)
+dim(gapminder)
+nrow(gapminder)
+summary(gapminder)
+
+head(gapminder$lifeExp)
+summary(gapminder$lifeExp)
+hist(gapminder$lifeExp)
+
+# now for categorical year
+summary(gapminder$year)
+table(gapminder$year)
+
+# categories stored as factor
+class(gapminder$continent)
+summary(gapminder$continent)
+nlevels(gapminder$continent)
+barplot(table(gapminder$continent))
 
